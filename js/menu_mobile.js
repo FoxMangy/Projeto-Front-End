aaf54ef585fba7dd2b18267cc96144278076dbaf
FoxMangy <adrianocorrea2003@gmail.com>
2026-07-08 07:30:59 -0300

    Padronização e melhor suporte responsivo mobile

---
+++ js/menu_mobile.js
@@ -0,0 +1,12 @@
+//Script de menu retrátil no mobile
+
+//Achando qual o botão que vai ser utilizado pelo script - salvo como botao
+const botao=document.getElementById("botao_menu");
+//Achando qual o ID vai ser modificado dentro desse script - salvo como cabecalho
+const cabecalho=document.getElementById("menu");
+//Quando o botão especificado receber um click, ele vai realizar o que está dentro dos {}
+botao.addEventListener("click", () => {
+    //Vai adicionar ou remover a tag "fechado" do que foi salvo como "cabecalho" 
+    //Logo cabecalho = ID menu = header / header -> header.fechado e vice versa
+    cabecalho.classList.toggle("fechado");
+});
