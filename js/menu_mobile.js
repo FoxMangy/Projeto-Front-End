053447d725fb4560f03abb9e2c046d7079c64652
FoxMangy <adrianocorrea2003@gmail.com>
2026-07-08 08:04:40 -0300

    Padronização e Suporte responsivo ao mobile

--- js/menu_mobile.js
+++ js/menu_mobile.js
@@ -3,7 +3,7 @@
 //Achando qual o botão que vai ser utilizado pelo script - salvo como botao
 const botao=document.getElementById("botao_menu");
 //Achando qual o ID vai ser modificado dentro desse script - salvo como cabecalho
-const cabecalho=document.getElementById("menu");
+const cabecalho=document.getElementById("main_menu");
 //Quando o botão especificado receber um click, ele vai realizar o que está dentro dos {}
 botao.addEventListener("click", () => {
     //Vai adicionar ou remover a tag "fechado" do que foi salvo como "cabecalho" 
